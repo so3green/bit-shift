c159ad615bff89bda003d915c84bf18efde66d4a
so3green <so3smile@yahoo.co.jp>
2024-09-23 13:31:41 +0900

    data code

--- bitshift.c
+++ bitshift.c
@@ -7,11 +7,13 @@ void main(){
     uint8_t flag = 0b00000001; //1bit
     uint8_t stop = 0b00000000; //1bit
 
-    uint8_t sendmsg = flag << 7;
+    uint8_t sendmsg = (flag << 7|Resivernm << 4|length << 2|setup << 1|stop);
+    /*
     sendmsg += Resivernm << 4;
     sendmsg += length << 2;
     sendmsg += setup << 1;
     sendmsg += stop;
+    */
     printf("%d\n",flag);
     printf("%d\n",Resivernm);
     printf("%d\n",length);
